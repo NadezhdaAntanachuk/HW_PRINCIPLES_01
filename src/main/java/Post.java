cab33b9086884119f2a5a0a1f64a450a4fa1a4e8
NadezhdaAntanachuk <8527132@mail.ru>
2023-09-10 23:22:52 +0300

    First commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+    public FormDate birthday;
+    public String name; // поле для имени
+    // добавьте другие поля
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+
+}
